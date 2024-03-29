db6480be600a7b8bb25b7bef3ac8bb55544622b2
Osamu Morozumi <osamumorozumi@gmail.com>
2024-03-29 14:57:54 +0800

    more macroeconomic formulas

--- macro_economics.py
+++ macro_economics.py
@@ -5,3 +5,35 @@ class MacroEconomics:
     @staticmethod
     def inflation_factor(price_1, price_2):
         return price_2 / price_1
+    
+
+    """
+    Calculate Aggregate Demand
+    """
+    @staticmethod
+    def aggregate_demand(consumption,investment,government_spending,exports,imports):
+        return consumption + investment + government_spending + (exports - imports)
+    
+    """
+    Calculate GDP using expenditure formula
+    """
+    @staticmethod
+    def expenditure_formula(consumption,investment,government_spending,exports,imports):
+        return MacroEconomics.aggregate_demand(consumption,investment,government_spending,exports,imports)
+    
+  
+    """
+    Calculate consumption multiplier based on marginal propensity to consume
+    (MPC)
+    """
+    @staticmethod
+    def consumption_multiplier(mpc):
+        return 1 / (1 - mpc)
+    
+    """
+    Calculate effect on economy of government spending based on marginal 
+    propensity to consume (MPC)
+    """
+    @staticmethod
+    def mpc_spending_effect(spending,mpc):
+        return spending * MacroEconomics.consumption_multiplier(mpc)
